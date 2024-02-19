5a975bd3c322e854d24a90bb208c9a2da71d02df
Ralph Fuentes <academy4@talentamp.com>
2024-02-19 16:47:56 +0800

    Currently at module 24

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>My Website</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
-          style={{
-            color: "white",
-            backgroundColor: "#43d2bd ",
-            borderRadius: "8px",
-          }}
-        >
-          New Blog
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
     </nav>
   );
